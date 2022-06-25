c1b4c680ec7117798a8b1093536034256926490a
Shakhzod Kudratov <sh.kudratov@uic.group>
2022-06-25 20:09:42 +0500

    close connection

--- database/db.py
+++ database/db.py
@@ -8,6 +8,7 @@ class MasterData:
         cur.execute(f'''INSERT INTO masters(`master_id`,`master_name`,`master_phone`)
                         VALUES ({master_id},"{master_name}","{master_phone}") ''')
         cnx.commit()
+        cnx.close()
 
     def update_master(master_id, master_name, master_phone):
         cnx, cur = create_connection()
@@ -15,12 +16,14 @@ class MasterData:
             f'''UPDATE masters SET `master_name`="{master_name}", `master_phone`="{master_phone}"
                         WHERE `master_id`={master_id} ''')
         cnx.commit()
+        cnx.close()
 
     def get_master(master_id):
         cnx, cur = create_connection()
         cur.execute(
             f''' SELECT * FROM `masters` WHERE `master_id` = {master_id}''')
         master = cur.fetchone()
+        cnx.close()
         return master
 
     def update_master_point(master_id):
@@ -28,12 +31,14 @@ class MasterData:
         cur.execute(
             f'''UPDATE masters SET `master_point` = `master_point` + 1 WHERE `master_id` = {master_id} ''')
         cnx.commit()
+        cnx.close()
 
     def minus_master_point(master_number, points):
         cnx, cur = create_connection()
         cur.execute(
             f'''UPDATE masters SET `master_point` = `master_point` - {points} WHERE `master_phone` = {master_number} ''')
         cnx.commit()
+        cnx.close()
 
 
 class AdminData:
@@ -41,6 +46,7 @@ class AdminData:
         cnx, cur = create_connection()
         cur.execute(f'''SELECT * FROM masters''')
         master = cur.fetchall()
+        cnx.close()
         return master
 
     def get_excel():
@@ -48,4 +54,5 @@ class AdminData:
         cur.execute(
             f'''SELECT `master_name`, `master_phone`, `master_point` FROM masters''')
         master = cur.fetchall()
+        cnx.close()
         return master
